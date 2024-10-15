cb75115c99b4e94fda3bb4e09629c90a8001ba3e
sarthkh <sarthakhandelwal@gmail.com>
2024-10-15 18:41:57 +0530

    initial project setup

---
+++ src/test/java/com/sarthkh/todoreminderapp/TodoReminderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarthkh.todoreminderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoReminderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
