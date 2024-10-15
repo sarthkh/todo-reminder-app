127288e9f2e9f3ba44d051671bea53416c826526
Sarthak Khandelwal <sarthakhandelwal@gmail.com>
2024-10-15 18:41:57 +0530

    initial project setup

---
+++ src/main/java/com/sarthkh/todoreminderapp/TodoReminderAppApplication.java
@@ -0,0 +1,13 @@
+package com.sarthkh.todoreminderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoReminderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoReminderAppApplication.class, args);
+    }
+
+}
