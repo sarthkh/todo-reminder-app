fba996cdc388525b04b2dd94b8c0fa884c7a3460
Sarthak Khandelwal <sarthakhandelwal@gmail.com>
2024-10-16 18:35:49 +0530

    add file storage, security, attachment logic

--- src/main/java/com/sarthkh/todoreminderapp/model/Todo.java
+++ src/main/java/com/sarthkh/todoreminderapp/model/Todo.java
@@ -1,27 +1,35 @@
 package com.sarthkh.todoreminderapp.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Title is required")
     @Column(nullable = false)
     private String title;
 
     private String description;
 
+    @NotNull(message = "Reminder date time is required")
     @Column(nullable = false)
     private LocalDateTime reminderDateTime;
 
+    @JsonManagedReference
     @OneToMany(mappedBy = "todo", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Attachment> attachments = new ArrayList<>();
 
